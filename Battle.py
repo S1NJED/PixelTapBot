d7ba5103d53b0ad9b614e96135701e0644f85488
de0ffuscated <161212435+de0ffuscated@users.noreply.github.com>
2024-06-18 11:11:25 +0700

    Add files via upload
    add improvment for auto upgrade pet can be setting on/off on config.json

--- Battle.py
+++ Battle.py
@@ -4,25 +4,24 @@ import json
 from random import randint
 from colorama import Fore, Style, Back
 from time import sleep
-from threading import Thread
 
 class Battle:
-    
-    def __init__(
-        self,
-    ):
+    """Represents a battle session in the Pixelverse game."""
+
+    def __init__(self):
+        """Initializes the Battle object with game settings."""
         self.url = 'https://api-clicker.pixelverse.xyz/api/users'
         with open('./config.json', 'r') as file:
             config = json.load(file)
-            
+
         self.secret = config['secret']
-        self.tgId = config['tgId'] # tg-id
+        self.tgId = config['tgId']
         self.initData = config['initData']
         self.hitRate = config['hitRate']
 
         self.websocket: websockets.WebSocketClientProtocol = None
         self.battleId = ""
-        self.superHit: bool = False
+        self.superHit = False
         self.strike = {
             "defense": False,
             "attack": False
@@ -30,15 +29,14 @@ class Battle:
 
         self.space = "                                        "
         self.stop_event = asyncio.Event()
-    
+
     async def sendHit(self):
+        """Continuously sends 'HIT' actions during the battle."""
         while not self.stop_event.is_set():
-            
-            # Stop sending hit if superHit phase occurs
             if self.superHit:
                 await asyncio.sleep(0.3)
                 continue
-            
+
             content = [
                 "HIT",
                 {
@@ -51,26 +49,26 @@ class Battle:
                 return
             await asyncio.sleep(self.hitRate)
 
-
     async def listenerMsg(self):
+        """Listens for and processes incoming messages from the game server."""
         while not self.stop_event.is_set():
             try:
                 data = await self.websocket.recv()
-            except:
+            except Exception as e:
+                print(f"{self.space}> Error in listenerMsg: {e}")
                 return
-            
+
             if data.startswith('42'):
-                data = json.loads(data.removeprefix('42'))
-                
-                # Change display
+                data = json.loads(data[2:])  # Remove prefix "42"
+
                 if data[0] == "HIT":
-                    
-                    print(f"{self.space}> {self.player1['name']} ({data[1]['player1']['energy']}) {Back.WHITE + Fore.BLACK}VERSUS{Style.RESET_ALL} ({data[1]['player2']['energy']}) {self.player2['name']}", end="\r", flush=True)
-                
+                    print(
+                        f"{self.space}> {self.player1['name']} ({data[1]['player1']['energy']}) {Back.WHITE + Fore.BLACK}VERSUS{Style.RESET_ALL} ({data[1]['player2']['energy']}) {self.player2['name']}",
+                        end="\r", flush=True)
+
                 elif data[0] == "SET_SUPER_HIT_PREPARE":
                     self.superHit = True
-                    pass
-                
+
                 elif data[0] == "SET_SUPER_HIT_ATTACK_ZONE":
                     content = [
                         "SET_SUPER_HIT_ATTACK_ZONE",
@@ -80,9 +78,8 @@ class Battle:
                         }
                     ]
                     await self.websocket.send(f"42{json.dumps(content)}")
-                    
                     self.strike['attack'] = True
-                    
+
                 elif data[0] == "SET_SUPER_HIT_DEFEND_ZONE":
                     content = [
                         "SET_SUPER_HIT_DEFEND_ZONE",
@@ -92,72 +89,56 @@ class Battle:
                         }
                     ]
                     await self.websocket.send(f"42{json.dumps(content)}")
-                    
                     self.strike['defense'] = True
-                    
-                
+
                 elif data[0] == "END":
                     result = data[1]['result']
                     reward = data[1]['reward']
 
                     sleep(0.3)
-                    print('') 
-                    print(f"{self.space}> You {Fore.WHITE}{Back.GREEN if result=='WIN' else Back.RED}{result}{Style.RESET_ALL} {Style.BRIGHT}{reward}{Style.RESET_ALL} coins !")
-                    
-                    # served send 41
-                    await self.websocket.recv()   
+                    print('')
+                    print(
+                        f"{self.space}> You {Fore.WHITE}{Back.GREEN if result == 'WIN' else Back.RED}{result}{Style.RESET_ALL} {Style.BRIGHT}{reward}{Style.RESET_ALL} coins !")
+
+                    await self.websocket.recv()
                     self.stop_event.set()
-                    # print(f"{self.space}> End of the match !")
                     return
-                
+
                 try:
-                    if ( self.strike['attack'] and not self.strike['defense'] ) or ( self.strike['defense'] and not self.strike['attack'] ):
-                        # Wait for result
+                    if (self.strike['attack'] and not self.strike['defense']) or (
+                            self.strike['defense'] and not self.strike['attack']):
                         await self.websocket.recv()
                         await self.websocket.recv()
-                        
-                
+
                     if self.strike['attack'] and self.strike['defense']:
-                        deux = await self.websocket.recv()
-                        await self.websocket.send("3")
-                        
-                        # SUPER_HIT_ROUND_RESULT
                         await self.websocket.recv()
-                        self.superHit = False          
+                        await self.websocket.send("3")
+                        await self.websocket.recv()
+                        self.superHit = False
                 except:
                     pass
-    
-    async def handleWssFreeze(self, seconds: int):
-        await asyncio.sleep(seconds)
-        
-        if self.stop_event.is_set():
-            return
-        
-        self.stop_event.set()
-        self.websocket.close()
-        print(f"bot wss has froze, bot is restarting ...")
-        
+
     async def connect(self):
+        """Establishes a connection to the game server and starts the battle."""
         uri = "wss://api-clicker.pixelverse.xyz/socket.io/?EIO=4&transport=websocket"
-        
+
         async with websockets.connect(uri) as websocket:
-            # print(f'{self.space}> Connected to the websockets.')
             self.websocket = websocket
-            
+
             data = await websocket.recv()
-            
-            # We authenticate ourselves ...
+
             content = {
                 "tg-id": self.tgId,
                 "secret": self.secret,
                 "initData": self.initData
             }
-            await websocket.send(f"40{json.dumps(content)}" )
-            
+            await websocket.send(f"40{json.dumps(content)}")
+
             await websocket.recv()
-            
             data = await websocket.recv()
-            data = json.loads(data.removeprefix('42'))
+
+            data = json.loads(data[2:])  # Remove prefix "42"
+
             self.battleId = data[1]['battleId']
             self.player1 = {
                 "name": data[1]['player1']['username']
@@ -167,14 +148,13 @@ class Battle:
             }
 
             for i in range(5, 0, -1):
-                print(f"{self.space}> The fight start in {Back.RED + Fore.WHITE}{i}{Style.RESET_ALL} seconds.", end="\r", flush=True)
+                print(
+                    f"{self.space}> The fight start in {Back.RED + Fore.WHITE}{i}{Style.RESET_ALL} seconds.",
+                    end="\r", flush=True)
                 await asyncio.sleep(1)
-            
+
             listenerMsgTask = asyncio.create_task(self.listenerMsg())
             hitTask = asyncio.create_task(self.sendHit())
-            handleWssFreeze = asyncio.create_task(self.handleWssFreeze(180))
-            
-            await handleWssFreeze
+
             await listenerMsgTask
             await hitTask
-            
--- main.py
+++ main.py
@@ -1,93 +1,102 @@
 from Battle import Battle
 from Pixelverse import UserPixel
+import json
 from random import randint
 import asyncio
-from colorama import Fore, Style, init, Back 
+from colorama import Fore, Style, init, Back
 import os
-from time import sleep
-import traceback
+import time
+import sys
 
 def clear():
+    """Clears the terminal screen."""
     if os.name == 'nt':
         os.system('cls')
     else:
         os.system('clear')
-clear()
 
 def split_chunk(var):
+    """Splits a string or number into chunks of 3, separated by spaces.
+
+    Args:
+        var: The string or number to be split.
+
+    Returns:
+        The formatted string with chunks of 3.
+    """
+    if isinstance(var, int):
+        var = str(var)
     n = 3
     var = var[::-1]
     return ' '.join([var[i:i + n] for i in range(0, len(var), n)])[::-1]
 
 async def main():
-    init() # colorama
-    
+    """Main asynchronous function to run the Pixelverse bot."""
+    init()
     user = UserPixel()
 
+    # Read config file 
+    with open('./config.json', 'r') as config_file:
+        config = json.load(config_file)
+
     while True:
-        userInfo = user.getUser()
-        stats = user.getStats()
-        
-        message = f"""
-                                    {Back.MAGENTA}{Fore.WHITE}PixelBot{Style.RESET_ALL} | Made by {Back.MAGENTA + Fore.WHITE}S1NJED{Style.RESET_ALL}
-                                    
-                                        Support me please :)
-                            {Fore.GREEN}$USDT{Style.RESET_ALL} (ERC20 or BEP20): {Back.WHITE + Fore.BLACK}0x0A9072E3C4Fae8e239Db12B3287eF88A3e9Da5A2{Style.RESET_ALL}
-                                                                                
-                                        Logged in as {Style.BRIGHT + Fore.GREEN}{userInfo['username']}{Style.RESET_ALL}{Fore.GREEN + Style.BRIGHT}
-                                         
-============================================== {Style.RESET_ALL} {Back.YELLOW + Fore.BLACK}STATS{Style.RESET_ALL} {Fore.GREEN + Style.BRIGHT} ==============================================  {Style.RESET_ALL}
+        try:
+            userInfo = user.getUser()
+            stats = user.getStats()
 
-                                        > {Back.YELLOW + Fore.BLACK}Balance{Style.RESET_ALL}: {Style.BRIGHT}{split_chunk(str(int(userInfo['clicksCount'])))}{Style.RESET_ALL}
+            message = f"""
+                                        {Back.MAGENTA}{Fore.WHITE}PixelBot{Style.RESET_ALL} | Made by {Back.MAGENTA + Fore.WHITE}S1NJED{Style.RESET_ALL}
 
-                                        > battlesCount{Style.RESET_ALL}: {Style.BRIGHT}{split_chunk(str(stats['battlesCount']))}{Style.RESET_ALL} 
-                                        > {Back.GREEN}Wins{Style.RESET_ALL}:         {Style.BRIGHT}{split_chunk(str(stats['wins']))}{Style.RESET_ALL}
-                                        > {Back.RED}Loses{Style.RESET_ALL}:        {Style.BRIGHT}{split_chunk(str(stats['loses']))}{Style.RESET_ALL}
-                                        > {Back.GREEN}Money Won{Style.RESET_ALL}:    {Style.BRIGHT}{split_chunk(str(stats['winsReward']))}{Style.RESET_ALL}
-                                        > {Back.RED}Money Lost{Style.RESET_ALL}:   {Style.BRIGHT}-{split_chunk(str(abs(stats['losesReward'])))}{Style.RESET_ALL}
-                                        > {Back.GREEN}Total earned{Style.RESET_ALL}: {Style.BRIGHT}{split_chunk(str(stats['winsReward'] - stats['losesReward']))}{Style.RESET_ALL}
+                                            Support me please :)
+                                {Fore.GREEN}$USDT{Style.RESET_ALL} (ERC20 or BEP20): {Back.WHITE + Fore.BLACK}0x0A9072E3C4Fae8e239Db12B3287eF88A3e9Da5A2{Style.RESET_ALL}
 
-        """
-        print(message)
-        
-        # We create a new Battle instance
-        battle = Battle()
-        
-        await battle.connect()
-        del battle
-        
-        user.claim()
-        user.upgradePets()
-        
-        timeToWait = randint(5, 10)
-        print(f"                                        > Waiting {Back.RED + Fore.WHITE}{timeToWait}{Style.RESET_ALL} seconds.")
-        await asyncio.sleep(timeToWait)
+                                            Logged in as {Style.BRIGHT + Fore.GREEN}{userInfo['username']}{Style.RESET_ALL}{Fore.GREEN + Style.BRIGHT}
 
-        clear()   
+    ============================================== {Style.RESET_ALL} {Back.YELLOW + Fore.BLACK}STATS{Style.RESET_ALL} {Fore.GREEN + Style.BRIGHT} ==============================================  {Style.RESET_ALL}
 
-''' TODO:
-POST https://api-clicker.pixelverse.xyz/api/pets/buy?tg-id=&secret=
-'''
+                                            > {Back.YELLOW + Fore.BLACK}Balance{Style.RESET_ALL}: {Style.BRIGHT}{split_chunk(str(int(userInfo['clicksCount'])))}{Style.RESET_ALL}
+
+                                            > battlesCount{Style.RESET_ALL}: {Style.BRIGHT}{split_chunk(str(stats['battlesCount']))}{Style.RESET_ALL}
+                                            > {Back.GREEN}Wins{Style.RESET_ALL}:         {Style.BRIGHT}{split_chunk(str(stats['wins']))}{Style.RESET_ALL}
+                                            > {Back.RED}Loses{Style.RESET_ALL}:        {Style.BRIGHT}{split_chunk(str(stats['loses']))}{Style.RESET_ALL}
+                                            > {Back.GREEN}Money Won{Style.RESET_ALL}:    {Style.BRIGHT}{split_chunk(str(stats['winsReward']))}{Style.RESET_ALL}
+                                            > {Back.RED}Money Lost{Style.RESET_ALL}:   {Style.BRIGHT}-{split_chunk(str(abs(stats['losesReward'])))}{Style.RESET_ALL}
+                                            > {Back.GREEN}Total earned{Style.RESET_ALL}: {Style.BRIGHT}{split_chunk(str(stats['winsReward'] - stats['losesReward']))}{Style.RESET_ALL}
+
+            """
+            print(message)
+
+            # Get auto-upgrade setting from config.json
+            auto_upgrade = config.get("upgrade", "on").lower() == "on"
+
+            # Battle logic 
+            battle = Battle()
+            await battle.connect()
+            del battle
+
+            user.claim()
+            user.upgradePets(auto_upgrade=auto_upgrade)  # Pass auto_upgrade choice
+
+            timeToWait = randint(5, 10)
+            print(f"{user.space}> Waiting {Back.RED + Fore.WHITE}{timeToWait}{Style.RESET_ALL} seconds.")
+            await asyncio.sleep(timeToWait)
+            clear()
+
+        except Exception as e:
+            print(f"{user.space}> Encountered an error: {type(e).__name__} - {e}")
+            print(f"{user.space}> Restarting in 5 seconds...")
+            await asyncio.sleep(5)
+            clear()
 
 if __name__ == '__main__':
     while True:
         try:
-            asyncio.run(main())       
+            asyncio.run(main())
         except KeyboardInterrupt:
-            print("                                        > Goodbye :)")
-            break
-        except Exception as err:
+            print(f"{UserPixel().space}> Goodbye :)")
+            sys.exit(0)
+        except Exception as e:
+            print(f"{UserPixel().space}> Critical error: {type(e).__name__} - {e}")
+            print(f"{UserPixel().space}> Restarting in 10 seconds...")
+            time.sleep(10)
             clear()
-            traceback.print_exc()
-            print("BOT HAS CRASHED :(")
-            pixelverse = UserPixel()
-            
-            if pixelverse.isBroken():
-                print(f"Pixelverse seems to be down for the moment, retrying again in 5 minutes ...")
-                sleep(60*5)
-            else:
-                print("The problem does not come from the server, try to restart the bot or if the problem persist open a ticket with a screen of this error ...")
-                
-                print("Trygin again in 60 seconds ...")
-                sleep(60)
-                
